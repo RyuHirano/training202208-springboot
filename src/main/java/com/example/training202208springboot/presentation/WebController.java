646ab4392ff785ac1476c4ac2f899d87636acffb
Ryu Hirano <r.hirano@jcdsol.co.jp>
2022-08-03 00:41:35 +0900

    Thymeleafまで完了

--- src/main/java/com/example/training202208springboot/presentation/WebController.java
+++ src/main/java/com/example/training202208springboot/presentation/WebController.java
@@ -1,5 +1,6 @@
 package com.example.training202208springboot.presentation;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class WebController {
-
   @GetMapping
-  public String get(Model model) {
+  public String index(Model model) {
 
     model.addAttribute("message", "こんにちは");
+    model.addAttribute("style", "background-color:red;");
+    model.addAttribute("list", List.of("山田", "田中", "佐藤"));
 
-    return "test";
+    return "index";
   }
 }
